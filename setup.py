515aa765306ace945dfddb4d6ae42637db66c2ae
reinanbr <perseu912@gmail.com>
2022-10-01 11:06:19 -0300

    start it beautifull project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='kitano',
+    version='0.0.1',
+    url='https://github.com/perseu912/kitano',
+    license='MIT License',
+    author='Reinan Br',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='slimchatuba@gmail.com',
+    keywords='kit tools dev works',
+    description=u'Library for development and works in python3.9+',
+    packages=find_packages(),
+    install_requires=['tqdm'],)
